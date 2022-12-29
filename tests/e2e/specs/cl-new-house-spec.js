a09da4277d9b440a24771e67207a2db5452f0727
Nelson Castillo <nelson.castillo@cryptoleague.org>
2022-12-29 05:31:12 -0500

    Shuffle stuff

---
+++ tests/e2e/specs/cl-new-house-spec.js
@@ -0,0 +1,91 @@
+/* eslint-disable ui-testing/no-disabled-tests */
+
+// 0x18D55201ac6EFcb4180B8f86d8CB5F524b81efA9
+const SECRET_WORDS =
+  'organ leopard helmet voice math cactus pitch else perfect firm matrix curtain';
+
+// before(() => {
+//   cy.disconnectMetamaskWalletFromAllDapps();
+// });
+
+describe('CryptoLeague', () => {
+  // beforeEach(() => {
+  //   cy.visit('/');
+  // });
+
+  context('Test commands', () => {
+    it(`First open`, () => {
+      cy.setupMetamask(SECRET_WORDS, 'goerli', 'Tester@1234').then(
+        setupFinished => {
+          expect(setupFinished).to.be.true;
+        },
+      );
+
+      cy.visit('/');
+      cy.switchToMetamaskWindow();
+
+      cy.acceptMetamaskAccess().then(connected => {
+        expect(connected).to.be.true;
+      });
+
+      cy.visit('/');
+      cy.switchToCypressWindow();
+
+      cy.confirmMetamaskSignatureRequest().then(confirmed => {
+        expect(confirmed).to.be.true;
+      });
+
+      cy.switchToCypressWindow();
+      cy.contains('Setup House').click();
+
+      cy.contains('NFT').click();
+
+      // TODO: Get by class
+      cy.get('input.CrInput-inputField').type('Synpress house');
+      cy.get('textarea.CrInput-inputField').type(
+        'You will be rich, I can tell',
+      );
+      cy.contains('Continue').click();
+
+      cy.contains('Select option').click();
+      cy.contains('Club').click();
+      cy.contains('Continue').click();
+
+      // Single portfolio manager
+      cy.contains('Next').click();
+
+      // Community
+      cy.get('[type="checkbox"]').click();
+      cy.get('div.CrTooltip')
+        .click()
+        .type('{rightarrow}{rightarrow}{rightarrow}{rightarrow}{rightarrow}');
+      cy.get('textarea.CrInput-inputField').type('No robots allowed');
+      cy.contains('Next').click();
+
+      // No fees
+      cy.contains('Next').click();
+
+      // Accept terms
+      cy.contains('Accept Terms').click();
+
+      //cy.get('div.CrButtonGroup mt-4').contains('Accept').click();
+
+      cy.get('div.CrModal-body')
+        .should('be.visible')
+        .contains('Accept')
+        .click();
+
+      cy.contains('Confirm & Continue').click();
+      cy.contains('Processing...'); // I removed wait 5000
+      cy.confirmMetamaskTransaction({
+        gasLimit: 1000000,
+        baseFee: 100,
+        priorityFee: 10,
+      }).then(txData => {
+        expect(txData.confirmed).to.be.true;
+      });
+
+      cy.wait(1000000);
+    });
+  });
+});
